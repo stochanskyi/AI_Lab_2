3be509375827fa5506448db9d321a2c256e41999
Marian Stochanskyi <stochan.andr@gmail.com>
2021-03-04 00:10:07 +0200

    Fixed arrays generator

--- array_generation.py
+++ array_generation.py
@@ -5,11 +5,15 @@ def float_array(from_value, to_value, size):
     return np.random.uniform(from_value, to_value, size)
 
 
-def int_array(from_value, to_value, size):
+def int_array(from_value, to_value, size, inclusive=True):
+    if inclusive:
+        to_value += 1
     return np.random.randint(from_value, to_value, size)
 
 
-def int_matrix(from_value, to_value, n, m):
+def int_matrix(from_value, to_value, n, m, inclusive=True):
+    if inclusive:
+        to_value += 1
     return np.random.randint(from_value, to_value, size=(n, m))
 
 
